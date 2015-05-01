ddc24a4c5df9370c68000bc4e61d1300ef117abc
Daniel Cohen <daniel@Daniels-MacBook-Pro.local>
2015-04-30 23:18:02 -0400

    created yelp extractor and added json->csv converter

--- yelp/start.py
+++ yelp/start.py
@@ -1,5 +1,15 @@
 import csv
-import json
+
+
+
+class rest_id:
+    def __init__(self,rest_id):
+        self.rest_id = rest_id
+        self.f_dates = []
+        self.f_stars = []
+        self.y_star = []
+        self.reviews = []
+        self.rev_dates = []
 
 def read_yelp_to_b():
     yelp_to_b = {}
@@ -21,6 +31,30 @@ def read_targets():
             targets[row[2]][row[1]] = row[3:]
     return targets
 
+def get_yelp_id():
+    '''returns dict of yelp_id -> rest_id class'''
+    yelp_rest = {}
+    tr = {}
+    with open("data/restaurant_ids_to_yelp_ids.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            yelp_rest[row[1]] = rest_id(row[0])
+            tr[row[0]] = row[1]
+    with open("data/train_labels.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+               yelp_rest[tr[row[2]]].f_stars.append([row[3],row[4],row[5]])
+               yelp_rest[tr[row[2]]].f_dates.append(row[1])
+    with open("data/yelp_academic_dataset_review.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[4] in yelp_rest:
+                yelp_rest[row[4]].reviews.append(row[2])
+                yelp_rest[row[4]].rev_dates.append(row[7])
+                yelp_rest[row[4]].y_star.append(row[6])
+
+    return yelp_rest
+
 # def read_reviews(yelp_to_b):
 #     with open("data/yelp_academic_dataset_review.json") as f:
 #         data = json.loads(f.read())
@@ -28,4 +62,5 @@ def read_targets():
 if __name__ == "__main__":
     yelp_to_b = read_yelp_to_b()
     targets = read_targets()
+    objects =  get_yelp_id()
     #reviews = read_reviews(yelp_to_b)
