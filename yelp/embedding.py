9b2eed50224845218263572629aa8ae21a093f3a
Daniel Cohen <daniel@Daniels-MacBook-Pro.local>
2015-05-01 21:36:24 -0400

    for linode

--- yelp/embedding.py
+++ yelp/embedding.py
@@ -6,11 +6,15 @@ import gensim
 def parse_sentences():
     data = start.get_yelp_id()
     word2vec_input = []
+    i = 0
     for place in data.itervalues():
         for review in place.reviews:
-            sents = nltk.tokenize.sent_tokenize(review.decode('utf-8'))
+            sents = nltk.tokenize.sent_tokenize(str(review.decode('utf-8')))
             for sent in sents:
-                word2vec_input.append(encode_unicode(nltk.tokenize.word_tokenize(sent)))
+                word2vec_input.append((nltk.tokenize.word_tokenize(sent)))
+
+        if i%100 == 0:
+            print "tokenizing"
     return word2vec_input
 
 def encode_unicode(words):
@@ -21,8 +25,9 @@ def encode_unicode(words):
 
 
 def train_word2vec(sentences):
+    print "training NN"
     model = gensim.models.Word2Vec(sentences, min_count=20,workers=2)
-    model.save('data/word2vecmodel')
+    model.save('word2vecmodel')
 
 
 if __name__ == "__main__":
