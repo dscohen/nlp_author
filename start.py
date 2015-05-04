baeeb51a4c219d9b246441b663c22642147075ac
Tyler Palsulich <tpalsulich@gmail.com>
2015-05-04 18:54:20 -0400

    Move nlp_nltk.py to start.py and clean up imports.

--- start.py
+++ start.py
@@ -1,115 +1,86 @@
-from sklearn import metrics
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import SGDClassifier
-from sklearn import svm
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from tornado import web, ioloop
-from tornado.log import enable_pretty_logging
-
 import argparse
-import json
-import logging
+import math
 import numpy
-import random
-import slate.provider
-import tornado
-import blog.provider
 
-def train(args):
-    # A provider should have a method get(), which returns a dict with three entries:
-    # "data", a list of the texts
-    # "tags", the authors of the text (in the same order)
-    # "idx", a map from a tags index to an author name.
+from nltk import compat
+from sklearn import svm
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import SGDClassifier, Ridge
+from sklearn.naive_bayes import MultinomialNB
 
-    if args.source == "slate":
-        provider = slate.provider.Provider()
-    elif args.source == "blog":
-        provider = blog.provider.Provider()
+import yelp.start
 
-    r = provider.get()
+def train(classif, vectorizer, train_set, sparse):
+    X, y = list(compat.izip(*train_set))
+    X = vectorizer.fit_transform(X)
+    return classif.fit(X, y)
 
-    pct_train = .8
-    num_train = int(len(r["data"]) * pct_train)
+# See http://www.nltk.org/_modules/nltk/classify/scikitlearn.html
+def classify_many(classif, vectorizer, featuresets, round = True):
+    X, _ = list(compat.izip(*featuresets))
+    X = vectorizer.transform(X)
+    results = classif.predict(X)
+    results = [numpy.round_(x).clip(0) for x in results]
+    return results
 
-    random.seed(42)
-    indices = range(len(r["data"]))
-    random.shuffle(indices)
+# See http://www.nltk.org/_modules/nltk/classify/util.html
+def accuracy(results, test_set):
+    correct = [l == r for ((fs, l), r) in zip(test_set, results)]
+    correct = [x[0] and x[1] and x[2] for x in correct]
+    if correct:
+        return float(sum(correct))/len(correct)
+    else:
+        return 0
 
-    r["data"] = [r["data"][i] for i in indices]
-    r["tags"] = [r["tags"][i] for i in indices]
+def dot(v1, v2):
+    return sum([x*y for x, y in zip(v1, v2)])
 
-    train_data  = r["data"][:num_train]
-    train_tags  = r["tags"][:num_train]
-
-    test_data  = r["data"][num_train:]
-    test_tags  = r["tags"][num_train:]
-
-    names = r["idx"]
-
-    # http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
-    if args.algorithm == "bayes":
-        clf = Pipeline([
-            ('vect', CountVectorizer()),
-            ('tfidf', TfidfTransformer()),
-            ('clf', MultinomialNB()),
-        ])
-    elif args.algorithm == "svm":
-        clf = Pipeline([
-            ('vect', CountVectorizer()),
-            ('tfidf', TfidfTransformer()),
-            ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)),
-        ])
-    elif args.algorithm == "lsvc":
-        clf = Pipeline([
-            ('vect', CountVectorizer()),
-            ('tfidf', TfidfTransformer()),
-            ('clf', svm.LinearSVC()),
-        ])
-
-    clf = clf.fit(train_data, train_tags)
-    predicted = clf.predict(test_data)
-
-    print(metrics.classification_report(test_tags, predicted, target_names=names))
-    print "%s accuracy: %.2f" % (args.algorithm.title(), numpy.mean(predicted == test_tags))
-
-    return clf, names
-
-class QueryHandler(web.RequestHandler):
-    def initialize(self, clf, names):
-        self.clf = clf
-        self.names = names
-
-    def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-
-    def get(self):
-        data = self.get_argument("snippet")
-
-        self.write(json.dumps({"result": names[clf.predict([data])]}))
+def rmsle(results, test_set):
+    W = [14, 16, 18]
+    result = sum([(math.log(dot(W, l) + 1) - math.log(dot(W, r) + 1))**2
+        for ((fs, l), r) in zip(test_set, results)])
+    result = (result / len(results)) ** .5
+    return result
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--source",    choices=["slate", "amazon", "blog"],
-        required=True, help="Source to use for training")
-    parser.add_argument("-a", "--algorithm", choices=["bayes", "svm", "lsvc"],
+    parser.add_argument("-a", "--algorithm", choices=["bayes", "svm", "lsvc", "gboost", "ridge"],
         required=True, help="Machine learning algorithm")
-    parser.add_argument("-p", "--port", default=55555, help="Port to start tornado server on.")
-    parser.add_argument("-S", "--server", default=False, action='store_true')
+    parser.add_argument("-s", "--submit", default=False, action='store_true',
+        help="Whether or not to prepare a submission (default = False)")
     args = parser.parse_args()
 
-    clf, names = train(args)
+    sparse = True
 
-    print names[clf.predict(["this is a test of a snippet being sent to a clf"])]
+    data = yelp.start.get_data()
 
-    if args.server:
-        enable_pretty_logging()
+    if args.algorithm == "bayes":
+        classif = MultinomialNB()
+    elif args.algorithm == "svm":
+        classif = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)
+    elif args.algorithm == "lsvc":
+        classif = svm.LinearSVC()
+    elif args.algorithm == "gboost":
+        classif = GradientBoostingClassifier()
+        sparse = False
+    elif args.algorithm == "ridge":
+        classif = Ridge()
 
-        application = web.Application([
-            (r"/query", QueryHandler, dict(clf = clf, names = names)),
-        ])
+    vectorizer = DictVectorizer(dtype=float, sparse=sparse)
 
-        logging.info("Started server on port %d" % (args.port))
-        application.listen(args.port)
-        ioloop.IOLoop.instance().start()
+    if args.submit:
+        classif = train(classif, vectorizer, data, sparse)
+        test_set = yelp.start.get_test_data()
+        results = classify_many(classif, vectorizer, test_set)
+        yelp.start.print_results(results)
+    else:
+        pct_train = .8
+        num_train = int(len(data) * pct_train)
+        train_set, test_set = data[:num_train], data[num_train:]
+
+        classif = train(classif, vectorizer, train_set, sparse)
+        results = classify_many(classif, vectorizer, test_set)
+
+        print "Perfect Accuracy:", accuracy(results, test_set)
+        print "Weighted RMSLE:", rmsle(results, test_set)
