929b73a94cc70165fbc5fdcc8bf807abbcdf0a5c
Tyler Palsulich <tpalsulich@gmail.com>
2015-05-05 14:48:03 -0400

    Fix building of result.

--- start.py
+++ start.py
@@ -27,7 +27,6 @@ def classify_many(classif, vectorizer, featuresets, round = True):
 # See http://www.nltk.org/_modules/nltk/classify/util.html
 def accuracy(results, test_set):
     correct = [l == r for ((fs, l), r) in zip(test_set, results)]
-    #correct = [x[0] and x[1] and x[2] for x in correct]
     if correct:
         return float(sum(correct))/len(correct)
     else:
@@ -71,23 +70,27 @@ if __name__ == "__main__":
     vectorizer = DictVectorizer(dtype=float, sparse=sparse)
 
     datas = yelp.start.get_data(embedding = args.feature)
+    results = []
     for stars in xrange(3):
         f = lambda x: [x[0],x[1][stars]]
         data = map(f,datas)
         if args.submit:
             classif = train(classif, vectorizer, data, sparse)
             test_set = yelp.start.get_test_data()
-            results = classify_many(classif, vectorizer, test_set)
-            yelp.start.print_results(results)
+            results.append(classify_many(classif, vectorizer, test_set))
         else:
             pct_train = .8
             num_train = int(len(data) * pct_train)
             train_set, test_set = data[:num_train], datas[num_train:]
             classif = train(classif, vectorizer, train_set, sparse)
-            results = classify_many(classif, vectorizer, test_set)
-            # Can only measure accuracy when testing, not submitting.
-            numpy.asarray(results)
-            results = numpy.transpose(results)
-            results = results.tolist()
-            print "Perfect Accuracy:", accuracy(results, test_set)
-            print "Weighted RMSLE:", rmsle(results, test_set)
+            results.append(classify_many(classif, vectorizer, test_set))
+    results = numpy.asarray(results)
+    results = numpy.transpose(results)
+    results = results.tolist()
+
+    # Can only measure accuracy when testing, not submitting.
+    if args.submit:
+        yelp.start.print_results(results)
+    else:
+        print "Perfect Accuracy:", accuracy(results, test_set)
+        print "Weighted RMSLE:", rmsle(results, test_set)
